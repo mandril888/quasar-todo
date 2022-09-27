e1b732db5bf4c543e7e9d05ce9eed2b45895d3c1
Arturo Rubio <arubio@seocom.agency>
2022-09-27 13:58:22 +0200

    delete tasks done

--- src/pages/ToDo.vue
+++ src/pages/ToDo.vue
@@ -1,13 +1,90 @@
 <template>
-  <q-page class="q-px-xl q-my-xl">
-    <h4>ToDo</h4>
+  <q-page class="q-pa-xl bg-grey-2 column">
+    <h4 class="q-mt-none">ToDo</h4>
+    <q-list>
+      <q-item
+        class="bg-white q-my-sm"
+        :class="{ 'done bg-green-2': task.done }"
+        v-for="(task, index) in tasks.tasks"
+        :key="task.title"
+        @click="task.done = !task.done"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-checkbox
+            v-model="task.done"
+            color="$primary"
+            class="no-pointer-events"
+          />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            @click.stop="deleteTask(index)"
+            flat
+            round
+            dense
+            color="primary"
+            icon="delete"
+          />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
+// import { defineComponent } from "vue";
 
-export default defineComponent({
-  name: "ToDo",
+// export default defineComponent({
+//   name: "ToDo",
+// });
+import { reactive } from "vue";
+import { useQuasar } from "quasar";
+
+const $q = useQuasar();
+
+const tasks = reactive({
+  tasks: [
+    {
+      title: "Task A",
+      done: false,
+    },
+    {
+      title: "Task B",
+      done: false,
+    },
+    {
+      title: "Task C",
+      done: false,
+    },
+  ],
 });
+
+function deleteTask(element) {
+  $q.dialog({
+    title: "Confirm",
+    message: "Really delete?",
+    cancel: true,
+    persistent: true,
+  }).onOk(() => {
+    tasks.tasks.splice(element, 1);
+    $q.notify({
+      message: "Task deleted",
+      color: "primary",
+    });
+  });
+}
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
